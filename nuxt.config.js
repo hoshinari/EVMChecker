9ecabacd059d7496a6d556bf20a94e21005d42bf
Ehama Seiya <31039439+hoshinari@users.noreply.github.com>
2021-12-26 18:28:57 +0900

    Add Page setting

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,5 +61,12 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+    router: {
+    base: '/EVMChecker/'
+  },
+
+  generate: {
+    dir: 'docs'
   }
 }
